d8c3a725101ff81284c8f0aa6a858f3c70a63c4a
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-01-10 17:34:15 +0100

    Update control.lua

--- control.lua
+++ control.lua
@@ -1,31 +1,46 @@
-local function destroy_all_corpses(corpses)
+local function destroy_all_corpses(info)
+  -- Parameters:
+  local corpses = info.corpses
+  -- Clears corpses, raising event if minable.
   for _, corpse in pairs(corpses) do
-    if not corpse.minable then corpse.destroy() goto continue end
-    corpse.destroy({raise_destroy = true}) -- raising event just in case
-    ::continue::
-  end
-end
-
-local function destroy_all_corpses_and_drop_items(surface, corpses)
-  for _, corpse in pairs(corpses) do
-    if not corpse.minable then corpse.destroy() goto continue end
-    for i = 1, 11 do
-      local single_inventory = corpse.get_inventory(i)
-      if single_inventory == nil then return end
-      surface.spill_inventory({
-        inventory     = single_inventory,
-        position      = corpse.position,
-        allow_belts   = false, -- dropped items will not be picked up by existing belts
-        enable_looted = true,  -- dropped items can be picked up simply by walking over them
+    if corpse.minable then
+      corpse.destroy({
+        raise_destroy = true -- just in case
       })
+    else
+      corpse.destroy()
     end
-    corpse.destroy({raise_destroy = true}) -- raising event just in case
-    ::continue::
   end
 end
 
+local function destroy_all_corpses_and_drop_any_items(info)
+  -- Parameters:
+  local surface = info.surface
+  local corpses = info.corpses
+  -- Clears corpses, raising event and dropping items if minable:
+  for _, corpse in pairs(corpses) do
+    if corpse.minable then
+      for i = 1, 11 do
+        local single_inventory = corpse.get_inventory(i)
+        if single_inventory == nil then return end
+        surface.spill_inventory({
+          inventory     = single_inventory,
+          position      = corpse.position,
+          allow_belts   = false,
+          enable_looted = true,
+        })
+      end
+      corpse.destroy({
+        raise_destroy = true -- just in case
+      })
+    else
+      corpse.destroy()
+    end
+  end
+end
 
 local function clear_area(info)
+  -- Parameters:
   local surface = info.surface
   local area    = info.area
   local range   = info.range or 0
@@ -35,15 +50,25 @@ local function clear_area(info)
     area.right_bottom.x = area.right_bottom.x + range
     area.right_bottom.y = area.right_bottom.y + range
   end
-
-  surface.destroy_decoratives({area = area})
-
-  local corpses = surface.find_entities_filtered({area = area, type = "character-corpse"})
+  -- Clear decoratitives:
+  surface.destroy_decoratives({
+    area = area
+  })
+  -- Clear corpses, optionally dropping any items:
+  local corpses = surface.find_entities_filtered({
+    area = area,
+    type = "character-corpse"
+  })
   if corpses == {} then return end
   if storage.settings.lawnmower_drop_minable_items then
-    destroy_all_corpses_and_drop_items(surface, corpses)
+    destroy_all_corpses_and_drop_any_items({
+      surface = surface,
+      corpses = corpses
+    })
   else
-    destroy_all_corpses(corpses)
+    destroy_all_corpses({
+      corpses = corpses}
+    )
   end
 end
 
@@ -69,9 +94,9 @@ script.on_event({
 }, function(event)
   local entity = event.entity or event.destination
   if entity == nil or
-    entity.type == "entity-ghost" or
-    entity.type == "tile-ghost" or
-    not entity.prototype.selectable_in_game then
+     entity.type == "entity-ghost" or
+     entity.type == "tile-ghost" or
+     not entity.prototype.selectable_in_game then
     return
   end
   clear_area({
@@ -91,7 +116,8 @@ local function cacheSettings()
     settings.global["lawnmower-drop-minable-items"].value
 end
 
-script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
+script.on_event(
+  defines.events.on_runtime_mod_setting_changed, function(event)
   if event.setting ~= "lawnmower-building-clear-range" and
      event.setting ~= "lawnmower-drop-minable-items" then
     return
