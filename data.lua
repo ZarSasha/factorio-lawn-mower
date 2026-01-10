36f2313e3efc2897c1cac878657e94d98a5dff12
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-01-10 18:17:04 +0100

    fix

--- data.lua
+++ data.lua
@@ -1,6 +1,16 @@
-require("prototypes.shortcuts")
-
 data:extend({
+  {
+    type = "shortcut",
+    name = "give-lawnmower",
+    order = "z",
+    action = "spawn-item",
+    item_to_spawn = "lawnmower-lawnmower",
+    style = "green",
+    icon = "__lawn-mower__/graphics/icons/lawn-mower-white-32.png",
+    small_icon = "__lawn-mower__/graphics/icons/lawn-mower-white-32.png",
+    icon_size = 32,
+    small_icon_size = 32,
+  },
   {
     type = "selection-tool",
     name = "lawnmower-lawnmower",
@@ -15,11 +25,6 @@ data:extend({
       cursor_box_type = "not-allowed",
       -- "blueprint-snap-rectangle"
     },
-    ["alt_select"] = {
-      mode = {"nothing"},
-      border_color = {r = 0.125, g = 0.447, b = 0.13, a = 0.051},
-      cursor_box_type = "not-allowed" -- "blueprint-snap-rectangle"
-    },
-    flags = { "only-in-cursor", "not-stackable", "spawnable" },
+    flags = { "only-in-cursor", "not-stackable", "spawnable" }
   }
 })
