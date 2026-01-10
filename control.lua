cc16f3489984855dc3ccb18a8389c7eab2e3077d
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-01-10 15:20:28 +0100

    Update control.lua

--- control.lua
+++ control.lua
@@ -12,20 +12,22 @@ local function clear_area(info)
 
   if not storage.settings.lawnmower_drop_minable_items then return end
   local corpses = surface.find_entities_filtered({area = area, type = "corpse"})
-  --if corpses == {} then return end
+  if corpses == {} then return end
   for _, corpse in pairs(corpses) do
     if not corpse.minable then goto continue end
-    local temp_inventory = game.create_inventory(0)
-    local corpse_position = corpse.position
-    corpse.mine({inventory = temp_inventory})
-    surface.spill_inventory({
-      inventory     = temp_inventory,
-      position      = corpse_position,
-      allow_belts   = false, -- needed?
-      enable_looted = true,  -- needed?
-    })
-    temp_inventory.destroy()
+    local pos = corpse.position
+    for i = 1, 11 do
+      local inv = corpse.get_inventory(i)
+      if not (inv and inv.valid) then return end
+      surface.spill_inventory({
+        inventory     = inv,
+        position      = pos,
+        allow_belts   = false, -- needed?
+        enable_looted = true,  -- needed?
+      })
+    end
     ::continue::
+    corpse.destroy({raise_destroy = true}) -- raises event just in case
   end
 end
 
