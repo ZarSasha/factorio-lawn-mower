71b034687b384d073ef60e7f9ba377c4db7b40fc
ZarSasha <135356158+ZarSasha@users.noreply.github.com>
2026-01-11 14:54:18 +0100

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,5 +1,12 @@
 data:extend({
   {
+    type = "item",
+    name = "lawnmower-lawnmower",
+    stack_size = 1,
+    auto_recycle = false,
+    hidden = true
+  },
+  {
     type = "shortcut",
     name = "lawnmower-give-lawnmower",
     order = "z",
@@ -17,23 +24,23 @@ data:extend({
     icon = "__lawn-mower__/graphics/icons/lawn-mower-white-32.png",
     icon_size = 32,
     icon_mipmaps = 0,
-    stack_size = 1,
+    flags = {"only-in-cursor", "not-stackable", "spawnable"},
     subgroup = "other",
-    ["select"] = {
+    stack_size = 1,
+    select = {
       mode = {"nothing"},
       border_color = {r = 0.125, g = 0.447, b = 0.13, a = 0.051},
       cursor_box_type = "not-allowed",
       started_sound = {filename = "__core__/sound/deconstruct-select-start.ogg"}
       --ended_sound won't trigger, runtime script needed
     },
-    ["alt_select"] = {
+    alt_select = {
       mode = {"nothing"},
       border_color = {r = 0.125, g = 0.447, b = 0.13, a = 0.301},
       cursor_box_type = "not-allowed",
       started_sound = {filename = "__core__/sound/deconstruct-cancel-start.ogg"},
       --ended_sound won't trigger, runtime script needed
-    },
-    flags = { "only-in-cursor", "not-stackable", "spawnable" }
+    }
     --skip_fog_of_war = true
   },
   { -- Sound definition for runtime script.
